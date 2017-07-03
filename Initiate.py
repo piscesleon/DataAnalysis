1534b636d1945bc01243ca1bb4f5b59320b7c392
Leon Wang <piscesleon@gmail.com>
2017-07-04 00:09:06 +0800

    till agg_counts...

--- Initiate.py
+++ Initiate.py
@@ -26,6 +26,8 @@ for r in time_zones[:10]:
     print("'" + (r) + "'")
 
 frame=DataFrame(records)
+
+'''
 # print(frame) #输出frame的摘要
 # print(frame['tz'].value_counts()) #对tz字段进行计数，并从大到小排列
 print(frame['tz'].value_counts()[:10]) #对tz字段进行计数，并从大到小排列，并取前10个数据
@@ -42,5 +44,17 @@ print('----------------------') #在输出的部分空出一行
 tz_counts=clean_tz.value_counts()
 tz_counts[:10].plot(kind='barh', rot=0)
 # plt.show() #此句用于在plot后显示图形
-pylab.show() #此句用于在plot后显示图形, plt为定义的pylab的别名
+# pylab.show() #此句用于在plot后显示图形, plt为定义的pylab的别名
+'''
 
+print(frame.a.dropna()) #丢掉na值后的frame中的a字段的值
+results=Series([x.split()[0] for x in frame.a.dropna()]) #分离后取第1个值，即浏览器型号
+print(results.value_counts()[:10]) #输出计数结果中的前10个值
+
+cframe=frame[frame.a.notnull()] #外面不套frame[]，会变成true / false值
+# print(cframe)
+operating_system=np.where(cframe['a'].str.contains('Windows'),'Windows','NotWindows') #根据a中是否含Windows，判断是否为Windows系统
+# print(operating_system[:10])
+by_tz_os=cframe.groupby(['tz',operating_system]) #根据timezone和operating_system对数据进行分组
+agg_counts=by_tz_os.size().unstack().fillna(0) #size用于对分组数据进行计数，unstack用于重组数据，fillna(0)指无效数据计0
+print(agg_counts[:10])
